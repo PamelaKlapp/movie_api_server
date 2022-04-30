3edac5bfc7503e19496c74bba36be36dd3e20994
PamelaKlapp <pameklapp@gmail.com>
2022-04-30 13:01:33 +0200

    format prettier+

--- index.js
+++ index.js
@@ -1,73 +1,72 @@
-const express = require('express'),
-      morgan = require('morgan')
+const express = require("express"),
+  morgan = require("morgan");
 
 const app = express();
 
-app.use(morgan('common')); 
+app.use(morgan("common"));
 
 let top10movies = [
   {
-    title: 'Spirited Away',
-    director: 'Hayao Miyazaki'
+    title: "Spirited Away",
+    director: "Hayao Miyazaki",
   },
   {
-    title: 'Django',
-    director: 'Quentin Tarantino'
+    title: "Django",
+    director: "Quentin Tarantino",
   },
   {
-    title: 'Eternal Sunshine of the Spotless Mind',
-    director: 'Michel Gondry'
+    title: "Eternal Sunshine of the Spotless Mind",
+    director: "Michel Gondry",
   },
   {
-    title: 'Amélie',
-    director: 'Jean-Pierre Jeunet'
+    title: "Amélie",
+    director: "Jean-Pierre Jeunet",
   },
   {
-    title: 'El secreto de sus ojos',
-    director: 'Juan José Campanella'
+    title: "El secreto de sus ojos",
+    director: "Juan José Campanella",
   },
   {
-    title: 'Catch me if you can',
-    director: 'Steven Spielberg'
+    title: "Catch me if you can",
+    director: "Steven Spielberg",
   },
   {
-    title: 'Ratatouille',
-    director: 'Brad Bird & Jan Pinkava'
-  },{
-    title: 'Legally Blond',
-    director: 'Robert Luketic'
+    title: "Ratatouille",
+    director: "Brad Bird & Jan Pinkava",
   },
   {
-    title: 'Miss Congeniality',
-    director: 'Donald Petrie'
+    title: "Legally Blond",
+    director: "Robert Luketic",
   },
   {
-    title: 'Good Bye Lenin!',
-    director: 'Wolfgang Becker'
-  }
+    title: "Miss Congeniality",
+    director: "Donald Petrie",
+  },
+  {
+    title: "Good Bye Lenin!",
+    director: "Wolfgang Becker",
+  },
 ];
 
 // GET requests
-app.get('/', (req, res) => {
-  res.send('These are my 10 TOP movies! Check them out!');
+app.get("/", (req, res) => {
+  res.send("These are my 10 TOP movies! Check them out!");
 });
 
-app.get('/movies', (req, res) => {
-    res.json(top10movies);
+app.get("/movies", (req, res) => {
+  res.json(top10movies);
 });
-
 
 //EXPRESS Static
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-
-//Error 
+//Error
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
-  });
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
 
 // listen for requests
 app.listen(8080, () => {
-  console.log('Your app is listening on port 8080.');
+  console.log("Your app is listening on port 8080.");
 });
