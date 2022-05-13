c9d850d888830944a8694465f864a3e03706cc9f
PamelaKlapp <pameklapp@gmail.com>
2022-05-13 09:21:41 +0200

    delete and clean code
    delete in-memoir uses and movies and old code.
    unify endpoints

--- index.js
+++ index.js
@@ -20,76 +20,6 @@ app.use(morgan("common"));
 
 mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
-let users = [
-  {
-    id: 1,
-    name: "Martin Schmidt",
-    favoriteMovies: [],
-  },
-  {
-    id: 2,
-    name: "Kim Kardashian",
-    favoriteMovies: ["Django Unchained"],
-  },
-];
-
-let movies = [
-  {
-    title: "Spirited Away",
-    description:
-      "During her family's move to the suburbs, a sullen 10-year-old girl wanders into a world ruled by gods, witches, and spirits, and where humans are changed into beasts.",
-    genreType: "Animation",
-    director: {
-      name: "Hayao Miyazaki",
-      birth: "1965",
-      death: false,
-    },
-  },
-  {
-    title: "Django Unchained",
-    description:
-      "With the help of a German bounty-hunter, a freed slave sets out to rescue his wife from a brutal plantation-owner in Mississippi.",
-    genreType: "Thriller",
-    director: {
-      name: "Quentin Tarantino",
-      birth: "1963",
-      death: false,
-    },
-  },
-  {
-    title: "Eternal Sunshine of the Spotless Mind",
-    director: "Michel Gondry",
-  },
-  {
-    title: "Amélie",
-    director: "Jean-Pierre Jeunet",
-  },
-  {
-    title: "El secreto de sus ojos",
-    director: "Juan José Campanella",
-  },
-  {
-    title: "Catch me if you can",
-    director: "Steven Spielberg",
-  },
-  {
-    title: "Ratatouille",
-    director: "Brad Bird & Jan Pinkava",
-  },
-  {
-    title: "Legally Blond",
-    director: "Robert Luketic",
-  },
-  {
-    title: "Miss Congeniality",
-    director: "Donald Petrie",
-  },
-  {
-    title: "Good Bye Lenin!",
-    director: "Wolfgang Becker",
-  },
-];
-
 // Request to GET all movies
 
 app.get("/movies", (req, res) => {
@@ -107,13 +37,6 @@ app.get("/movies", (req, res) => {
 //Request to GET a single movie
 
 app.get("/movies/:Title", (req, res) => {
-  //const { movieTitle } = req.params;
-  //const movie = movies.find( movie => movie.title === movieTitle );
-  // if (movie) {
-  //   res.status(200).json(movie);
-  // } else {
-  //   res.status(404).send("Not found the movie");
-  // }
   Movies.findOne({Title: req.params.Title})
   .then((movies) => {
     res.json(movies);
@@ -127,16 +50,8 @@ app.get("/movies/:Title", (req, res) => {
 
 //Request to GET a single genre
 
-app.get("/movies/genre/:genreName", (req, res) => {
-//   const { genreName } = req.params;
-//   const genre = movies.find( movie => movie.genre === genreName ).genre;
-//   if (genre) {
-//     res.status(200).json(genre);
-//   } else {
-//     res.status(404).send("There is no movie with this genre");
-//   }
-// });
-Movies.findOne({ "Genre.Name": req.params.genreName })
+app.get("/movies/genre/:Name", (req, res) => {
+Movies.findOne({ "Genre.Name": req.params.Name })
 .then((movie) => {
   res.json(movie.Genre);
 })
@@ -149,23 +64,14 @@ Movies.findOne({ "Genre.Name": req.params.genreName })
 
 //Request to GET the director's name 
 
-app.get("/movies/director/:directorName", (req, res) => {
-//   const { directorName } = req.params;
-//   const director = movies.find( movie => movie.director.name === directorName ).director;
-  
-//   if (director) {
-//     res.status(200).json(director);
-//   } else {
-//     res.status(404).send("There is no director with that name");
-//   }
-// });
-Movies.findOne({ "Director.Name": req.params.directorName })
+app.get("/movies/director/:Name", (req, res) => {
+Movies.findOne({ "Director.Name": req.params.Name })
 .then((movie) => {
   res.json(movie.Director);
 })
 .catch((err) =>{
   console.error(err);
-  res.status(404).send("There is no genre with that name")
+  res.status(404).send("There is no director with that name")
 })
 });
 
@@ -173,16 +79,6 @@ Movies.findOne({ "Director.Name": req.params.directorName })
 // Create a new user
 
 app.post("/users", (req, res) => {
-//   const newUser = req.body;
-
-//   if (newUser.name) {
-//     newUser.id = uuid.v4();
-//     users.push(newUser);
-//     res.status(201).json(newUser)
-//   }else {
-//     res.status(400).send("It is required a name")
-//   }
-// })
   Users.findOne({Username: req.body.Username})
   .then((user) =>{
     if(user){
@@ -212,20 +108,9 @@ app.post("/users", (req, res) => {
 
 //Update username
 
-app.put("/users/:username", (req, res) => {
-  // const { id } = req.params;
-  // const updatedUser = req.body;
-
-  // let user = users.find( user => user.id == id );
-
-  // if (user){
-  //   user.name = updatedUser.name;
-  //   res.status(200).json(user);
-  // }else {
-  //   res.status(404).send("Update was not possible")
-  // }
+app.put("/users/:Username", (req, res) => {
   Users.findOneAndUpdate(
-    {Username: req.params.username},
+    {Username: req.params.Username},
     {$set:{
       Username: req.body.Username,
       Password: req.body.Password,
@@ -250,20 +135,10 @@ app.put("/users/:username", (req, res) => {
 
 //Add movie to favorites
 
-app.post("/users/:username/favorites/:Title", (req, res) => {
-  // const { id, movieTitle } = req.params;
-
-  // let user = users.find( user => user.id == id );
-
-  // if (user){
-  //   user.favoriteMovies.push(movieTitle)
-  //   res.status(200).send(`${movieTitle} has been added to user ${id} favorite List`);
-  // }else {
-  //   res.status(404).send("Movie was not possible to add to favorite List")
-  // }
-  Users.findOneAndUpdate({Username: req.params.username},
+app.post("/users/:Username/favorites/:MovieID", (req, res) => {
+  Users.findOneAndUpdate({Username: req.params.Username},
     {$push:
-      { favoriteMovies: req.params.Title}
+      { favoriteMovies: req.params.MovieID}
     },
     {new:true},
     (err, updatedUser) =>{
@@ -280,18 +155,8 @@ app.post("/users/:username/favorites/:Title", (req, res) => {
 
 //DELETE movie from favorites
 
-app.delete("/users/:username/favorites/:MovieID", (req, res) => {
-  // const { id, movieTitle } = req.params;
-
-  // let user = users.find( user => user.id == id );
-
-  // if (user){
-  //   user.favoriteMovies = user.favoriteMovies.filter( title => title !== movieTitle );
-  //   res.status(200).send(`${movieTitle} has been deleted from user ${id} favorite List`);
-  // }else {
-  //   res.status(404).send("Movie was not possible to remove from favorite List")
-  // }
-  Users.findOneAndUpdate({Username: req.params.username},
+app.delete("/users/:Username/favorites/:MovieID", (req, res) => {
+  Users.findOneAndUpdate({Username: req.params.Username},
     {$pull:
       { favoriteMovies: req.params.MovieID}
     },
@@ -310,23 +175,13 @@ app.delete("/users/:username/favorites/:MovieID", (req, res) => {
 
 //DELETE user
 
-app.delete("/users/:username", (req, res) => {
-  // const { id } = req.params;
-
-  // let user = users.find( user => user.id == id );
-
-  // if (user){
-  //   users = users.filter( user => user.id != id )
-  //   res.status(200).send(`${user.name} has been remove from platform`);
-  // }else {
-  //   res.status(404).send("User is stuck with us!")
-  // }
-  Users.findOneAndRemove({Username: req.params.username})
+app.delete("/users/:Username", (req, res) => {
+  Users.findOneAndRemove({Username: req.params.Username})
   .then((user) =>{
     if(!user){
-      res.status(400).send(req.params.username + " was not found.");
+      res.status(400).send(req.params.Username + " was not found.");
     }else {
-      res.status(200).send(req.params.username + " was deleted");
+      res.status(200).send(req.params.Username + " was deleted");
     }
   })
   .catch((err) =>{
